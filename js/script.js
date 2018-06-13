cb577b43a50921867ab5c90d79f445d3d87b176b
christian <gudim87@hotmail.com>
2018-06-13 15:26:08 +0200

    Added typed.js to a under text with a loop, and starting the about section

--- js/script.js
+++ js/script.js
@@ -5,4 +5,12 @@ $(document).ready(function() {
     play: 5000,
     pagination: false
   });
+
+  const typed = new Typed('.typed', {
+    strings: ['Junior Programmer.',"Student.", 'Looking for a junior position.'],
+    typeSpeed: 60,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
 });
