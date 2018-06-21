b0de2582a6adc2964579849055ba893005312e5b
christian <gudim87@hotmail.com>
2018-06-21 17:57:21 +0200

    Added preloader for when i refresh the page, added navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+$(window).on('load', function(){
+  $('.loader').fadeOut(500);
+});
+
 $(document).ready(function() {
 
   $('#slides').superslides({
@@ -66,4 +70,66 @@ $(document).ready(function() {
 
     }
   }); 
+
+  $('[data-fancybox]').fancybox();
+
+  $('.items').isotope( {
+    filter: '*',
+    anaimationOptions: {
+      duration: 1500,
+      easing: 'linear',
+      queue: false
+    }
+  });
+
+  $('#filters a').click(function() {
+    $('#filters.current').removeClass('current');
+    $(this).addClass('current');
+
+    const selector = $(this).attr('data-filter');
+
+    $('.items').isotope( {
+      filter: selector,
+      anaimationOptions: {
+        duration: 1500,
+        easing: 'linear',
+        queue: false
+    }
+  });
+
+    return false;
+
+  });
+
+  $('#navigation li a').click(function(e) {
+    e.preventDefault();
+
+    let targetElement = $(this).attr('href');
+    let targetPosition = $(targetElement).offset().top;
+    $('html, body').animate({ scrollTop: targetPosition - 50 }, 'slow');
+  });
+
+  const nav = $('#navigation');
+  const navTop = nav.offset().top;
+
+  $(window).on('scroll', stickyNavigation);
+
+  function stickyNavigation() {
+
+    let body = $('body');
+
+    if($(window).scrollTop() >= navTop) {
+      body.css('padding-top', nav.outerHeight() + "px");
+      body.addClass('fixedNav');
+    } else {
+      body.css('padding-top', 0);
+      body.removeClass('fixedNav');
+    }
+  }
+
+
+
+
+
+
 });
